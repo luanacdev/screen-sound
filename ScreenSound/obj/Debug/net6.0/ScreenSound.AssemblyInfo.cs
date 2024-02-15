e6b73c4841863a17e0caff73945f57544a9c5483
luanacdev <luana.cam20@gmail.com>
2024-02-15 19:03:48 -0300

    Generics

--- ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6300d30ea97f0b2fa5b7acdd269dcbba8ba538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcbde136fa66dd96682e0d307605d90e4c91e0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
